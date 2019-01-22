29de02ac2e84820db10a1a0eee7cd7eeead1a523
Anthony Kiplimo <anthonylimo90@gmail.com>
2019-01-22 09:55:31 +0300

    Venv changes

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template, request, json
 import africastalking
 
 #authenticate with africastalking
+username = "sandbox"
+api_key = "f6c6e1d0686fddddb375d47c8bcda0dc80713f214ce26b912567ab156ae7feee"
+africastalking.initialize(username,api_key)
 
 #initialize SMS service
+
+sms = africastalking.SMS
 
 #create flask app
 
@@ -13,6 +18,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
+    _sms_message = request.form["smsMessage"]
+    _phone_number = request.form["phoneNumber"]
+
+    response = sms.send(_sms_message, [str(_phone_number)])
+    #print(response)
     return render_template('index.html')
 
 @app.route("/showSMSPage")
@@ -25,13 +35,17 @@ def showSendSMS():
 @app.route("/sendSMS", methods=["POST"])
 def sendSMS():
     #Reading values from the UI
-    _sms_message = request.form[]
-    _phone_number = request.form[]
+    if request.method == "POST":
+        _sms_message = request.form["smsMessage"]
+        _phone_number = request.form["phoneNumber"]
+
+        response = sms.send(_sms_message, [str(_phone_number)])
+        print(response)
     
-    if _sms_message and _phone_number:
-        return json.dumps({'html':'<span>All fields are good. Sending SMS</span>'})
-    else:
-        return json.dumps({'html':'<span>Please fill the required fields</span>'})
+        if _sms_message and _phone_number:
+            return json.dumps({'html':'<span>All fields are good. Sending SMS</span>'})
+        else:
+            return json.dumps({'html':'<span>Please fill the required fields</span>'})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
